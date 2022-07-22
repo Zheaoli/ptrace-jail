7bc12d751225c12b326c7251ab9af1ce33bda8ce
BlackKD <X@qq.com>
2022-07-22 10:34:32 +0800

    add comment

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ from setuptools import Extension, find_packages, setup
 
 from jail import __version__
 
-subprocess.call("make")
+subprocess.call("make") #use cython for core.pyx to core.c
 
 extensions = [
     Extension(
+        # gcc jail/core/core.c -std=c99 -g
         "jail/core/core", ["jail/core/core.c"], extra_compile_args=["-std=c99", "-g"]
     )
 ]
@@ -19,7 +20,9 @@ setup(
     packages=find_packages("./"),
     include_package_data=True,
     install_requires=["cython", "psutil", "toml", "loguru"],
+    #start process = jail.cmd.jail_cmd:main
     entry_points={"console_scripts": {"pjail = jail.cmd.jail_cmd:main"}},
     version=__version__,
+    #to gcc *.c for ext_modules
     ext_modules=cythonize(extensions, language=3),
 )
